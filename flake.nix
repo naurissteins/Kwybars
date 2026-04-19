113b82646d87e746f506888248a4272d30c446fb
Oriol <142458783+oornaque@users.noreply.github.com>
2026-04-19 12:39:48 +0200

    add nix package

--- flake.nix
+++ flake.nix
@@ -11,6 +11,40 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
+        packages.kwybars = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "kwybars";
+            version = "0.1.6";
+
+            src = pkgs.fetchFromGitHub {
+                owner = "naurissteins";
+                repo = "Kwybars";
+                rev = version;
+                hash = "sha256-NAy8dA5iGNBtdoZ38LC72yStCQ9whGgT+rajyHYeZkA=";
+            };
+
+            cargoHash = "sha256-3wehddHWMy83NUTtAmTtbLS3k8jf75HI09qAHEQ5adc=";
+
+            buildInputs = with pkgs; [
+                cargo
+                rustc
+                gtk4
+                gtk4-layer-shell
+                pipewire
+                cava
+                libnotify
+            ];
+
+            nativeBuildInputs = with pkgs; [
+                pkg-config
+                makeWrapper
+            ];
+
+            postInstall = ''
+                wrapProgram $out/bin/kwybars-daemon --prefix PATH : ${pkgs.cava}/bin:${pkgs.libnotify}/bin:$out/bin
+                wrapProgram $out/bin/kwybars-overlay --prefix PATH : ${pkgs.cava}/bin:${pkgs.libnotify}/bin:$out/bin
+            '';
+        };
+        defaultPackage = self.packages.${system}.kwybars;
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             # Rust toolchain
